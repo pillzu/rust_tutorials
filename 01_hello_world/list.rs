0bddc8ab1b90cb7efceb0f02c6cc9d625b89d185
pillzu <piyushupadhyay2566@gmail.com>
2025-11-15 14:39:14 -0500

    01_hello_world done

---
+++ 01_hello_world/list.rs
@@ -0,0 +1,36 @@
+use std::fmt; // Import the `fmt` module.
+
+// Define a structure named `List` containing a `Vec`.
+#[derive(Debug)]
+struct List(Vec<i32>);
+
+// activity requires changing program to output index as well
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing,
+        // and create a reference to `vec`.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `v` in `vec` while enumerating the iteration
+        // index in `index`.
+        for (index, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator to return on errors.
+            if index != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{index}: {}", v)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+    println!("{:?}", v);
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+### Summary
+
+I am a rust noob as of Nov 15, 2025.
+
+In order to get up to speed, this repository goes through each chapter of Rust
+by Examples with the hope that I can become a rustacean myself soon:
+
+- <https://doc.rust-lang.org/rust-by-example/index.html>
+
+Wish me luck.

e59884f335cc800b75f0ab0fdbc6306ae153bbb2
pillzu <piyushupadhyay2566@gmail.com>
2025-11-20 11:34:11 -0500

    04: chapter completed

---
+++ 04_variable_binding/declare_first.rs
@@ -0,0 +1,27 @@
+/*
+* This is a way to declare and initialize variable separately but it does not seem like a very good
+* idea considering how much it affects readability.
+*/
+fn main() {
+    // Declare a variable binding
+    let a_binding;
+
+    {
+        let x = 2;
+
+        // Initialize the binding
+        a_binding = x * x;
+    }
+
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+    // Error! Use of uninitialized binding
+    // println!("another binding: {}", another_binding);
+    // FIXME ^ Comment out this line
+
+    another_binding = 1;
+
+    println!("another binding: {}", another_binding);
+}

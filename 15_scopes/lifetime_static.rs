307e9287d15cc4365abeb788adc31618ffb8169f
pillzu <piyushupadhyay2566@gmail.com>
2025-12-29 21:44:39 -0500

    ch 15: complete - lifetime annotations are confusing but will revisit if I encounter them again

---
+++ 15_scopes/lifetime_static.rs
@@ -0,0 +1,46 @@
+// Make a constant with `'static` lifetime.
+static NUM: i32 = 18;
+
+// Returns a reference to `NUM` where its `'static`
+// lifetime is coerced to that of the input argument.
+fn coerce_static<'a>(_: &'a i32) -> &'a i32 {
+    &NUM
+}
+
+fn main() {
+    {
+        // Make a `string` literal and print it:
+        let static_string = "I'm in read-only memory";
+        println!("static_string: {}", static_string);
+
+        // When `static_string` goes out of scope, the reference
+        // can no longer be used, but the data remains in the binary.
+    }
+
+    {
+        // Make an integer to use for `coerce_static`:
+        let lifetime_num = 9;
+
+        // Coerce `NUM` to lifetime of `lifetime_num`:
+        let coerced_static = coerce_static(&lifetime_num);
+
+        println!("coerced_static: {}", coerced_static);
+    }
+
+    println!("NUM: {} stays accessible!", NUM);
+
+    // example
+    use std::fmt::Debug;
+
+    fn print_it(input: impl Debug + 'static) {
+        println!("'static value passed in is: {:?}", input);
+    }
+
+    // i is owned and contains no references, thus it's 'static:
+    let i = 5;
+    print_it(i);
+
+    // oops, &i only has the lifetime defined by the scope of
+    // main(), so it's not 'static:
+    // print_it(&i);
+}
---
+++ 15_scopes/owernship_partial_moves.rs
@@ -0,0 +1,35 @@
+// NOTE:  If Person.age were stored on the stack, ref would not be required as the definition of
+// age would copy the data from person.age without moving it.
+fn main() {
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: Box<u8>,
+    }
+
+    // Error! cannot move out of a type which implements the `Drop` trait
+    //impl Drop for Person {
+    //    fn drop(&mut self) {
+    //        println!("Dropping the person struct {:?}", self)
+    //    }
+    //}
+    // TODO ^ Try uncommenting these lines
+
+    let person = Person {
+        name: String::from("Alice"),
+        age: Box::new(20),
+    };
+
+    // `name` is moved out of person, but `age` is referenced
+    let Person { name, ref age } = person;
+
+    println!("The person's age is {}", age);
+
+    println!("The person's name is {}", name);
+
+    // Error! borrow of partially moved value: `person` partial move occurs
+    // println!("The person struct is {:?}", person);
+
+    // `person` cannot be used but `person.age` can be used as it is not moved
+    println!("The person's age from person struct is {}", person.age);
+}
